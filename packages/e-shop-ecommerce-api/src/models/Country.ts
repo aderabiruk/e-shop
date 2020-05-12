a986f0aa011a443e5ef52b61201a38431a38357a
Biruk Adera <aderabiruk@gmail.com>
2020-05-12 23:19:17 +0300

    Country Model Prepared

---
+++ packages/e-shop-ecommerce-api/src/models/Country.ts
@@ -0,0 +1,49 @@
+import { model, Schema, Document } from 'mongoose';
+
+export interface Country extends Document {
+    name: string;
+    code: string;
+    flag: string;
+    currency_name: string;
+    currency_code: string;
+    created_at: Date;
+    updated_at: Date;
+    deleted_at: Date;
+};
+
+let CountrySchema = new Schema({ 
+    name: {
+		type: String,
+        required: true
+    },
+	code: {
+		type: String,
+		required: true
+    },
+    flag: {
+		type: String,
+		required: true
+    },
+    currency_name: {
+		type: String,
+		required: true
+    },
+    currency_code: {
+		type: String,
+		required: true
+    },
+	created_at: {
+		type: Date,
+		default: Date.now,
+    },
+    updated_at: {
+		type: Date,
+		default: Date.now,
+	},
+	deleted_at: {
+		type: Date,
+		default: null
+	}
+}, {collection: 'countries'});
+
+export default model<Country>('Country', CountrySchema);
