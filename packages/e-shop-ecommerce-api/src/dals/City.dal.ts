2d89a4de482f3a31bf1e6a354784ab992d858bf9
Biruk Adera <aderabiruk@gmail.com>
2020-05-13 00:26:44 +0300

    City DAL Prepared

---
+++ packages/e-shop-ecommerce-api/src/dals/City.dal.ts
@@ -0,0 +1,167 @@
+import transform_mongoose_error from 'mongoose-validation-error-handler';
+
+import City, { City as ICity } from '../models/City';
+
+class CityDAL {
+
+    /**
+     * Create City
+     * 
+     * @param {string} name
+     * @param {string} code
+     * @param {string} country_id
+     * @param {number} latitude
+     * @param {number} longitude
+     * 
+     * @returns {Promise<ICity>}
+     */
+    static create(name: string, code: string, country_id: string, latitude: number, longitude: number): Promise<ICity> {
+        return new Promise((resolve, reject) => {
+            let city = new City();
+            city.name = name;
+            city.code = code;
+            city.country_id = country_id;
+            city.location = {
+                type: "Point",
+                coordinates: [ longitude, latitude ]
+            };
+            city.save((error, savedCity) => {
+                if (error) {
+                    reject(transform_mongoose_error(error, { capitalize: true, humanize: true }))
+                }
+                else {
+                    resolve(savedCity);
+                }
+            });
+        });
+    }
+
+    /**
+     * Find Many Cities
+     * 
+     * @param {any}     query 
+     * @param {number}  page 
+     * @param {number}  limit 
+     * 
+     * @returns {Promise<ICity[]>}
+     */
+    static findMany(query: any, page: number = 1, limit: number = 25): Promise<ICity[]> {
+        return new Promise((resolve, reject) => {
+            City.find(query).limit(limit).skip((page - 1) * limit).exec((error, cities) => {
+                if (error) {
+                    reject(transform_mongoose_error(error, { capitalize: true, humanize: true }))
+                }
+                else {
+                    resolve(cities);
+                }
+            });
+        });
+    }
+
+    /**
+     * Find One Store
+     * 
+     * @param {any} query 
+     * 
+     * @returns {Promise<ICity>}
+     */
+    static findOne(query: any): Promise<ICity> {
+        return new Promise((resolve, reject) => {
+            City.findOne(query).exec((error, city) => {
+                if (error) {
+                    reject(transform_mongoose_error(error, { capitalize: true, humanize: true }))
+                }
+                else {
+                    resolve(city);
+                }
+            });
+        });
+    }
+
+    /**
+     * Update City
+     * 
+     * @param {City}   city 
+     * @param {any}     payload
+     * 
+     * @returns {Promise<ICity>}
+     */
+    static update(city: ICity, payload: any): Promise<ICity> {
+        return new Promise((resolve, reject) => {
+            if (city) {
+                city.name = payload.name ? payload.name : city.name;
+                city.code = payload.code ? payload.code : city.code;
+                city.country_id = payload.country_id ? payload.country_id : city.country_id;
+
+                if (payload.latitude && payload.longitude) {
+                    city.location = {
+                        type: "Point",
+                        coordinates: [ payload.longitude, payload.latitude ]
+                    };
+                }
+                
+                city.updated_at = new Date();
+                city.save((error, updatedCity) => {
+                    if (error) {
+                        reject(transform_mongoose_error(error, { capitalize: true, humanize: true }))
+                    }
+                    else {
+                        resolve(updatedCity);
+                    }
+                });
+            }
+            else {
+                resolve(null)
+            }
+        });
+    }
+
+    /**
+     * Delete City (Hard)
+     * 
+     * @param {any} query 
+     * 
+     * @returns {Promise<any>}
+     */
+    static deleteHard(query: any): Promise<any> {
+        return new Promise((resolve, reject) => {
+            City.deleteOne(query).exec((error, result) => {
+                if (error) {
+                    reject(transform_mongoose_error(error, { capitalize: true, humanize: true }))
+                }
+                else {
+                    resolve(result);
+                }
+            });
+        });
+    }
+
+    /**
+     * Delete City (Soft)
+     * 
+     * @param {City} city 
+     * 
+     * @returns {Promise<any>}
+     */
+    static deleteSoft(city: ICity): Promise<ICity> {
+        return new Promise((resolve, reject) => {
+            if (city) {
+                city.deleted_at = new Date();
+                city.save((error, deletedCity) => {
+                    if (error) {
+                        reject(transform_mongoose_error(error, { capitalize: true, humanize: true }))
+                    }
+                    else {
+                        resolve(deletedCity);
+                    }
+                });
+            } 
+            else {
+                resolve(null);
+            } 
+        });
+    }
+
+};
+
+export default CityDAL;
