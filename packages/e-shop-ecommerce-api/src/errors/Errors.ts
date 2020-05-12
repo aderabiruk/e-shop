b3767a1775070129b2e1b120d49934f037eaa9ef
Biruk Adera <aderabiruk@gmail.com>
2020-05-12 23:23:18 +0300

    Errors Utilities Prepared

---
+++ packages/e-shop-ecommerce-api/src/errors/Errors.ts
@@ -0,0 +1,80 @@
+import moment from 'moment';
+
+import Messages from './Messages';
+
+export class Error {
+    payload: any;
+    statusCode: number;
+
+    constructor(statusCode: number, errors: any[]) {
+        this.statusCode = statusCode;
+        this.payload = {
+            timestamp: moment(),
+            errors: errors
+        }
+    }
+};
+
+/**
+ * Bad Request Error
+ */
+export class BadRequestError extends Error {
+    constructor(errors: any[]) {
+        super(400, errors);
+    }
+};
+
+/**
+ * Unauthorized Error
+ */
+export class UnauthorizedError extends Error {
+    constructor() {
+        super(401, [
+            Messages.UNAUTHORIZED_ERROR
+        ]);
+    }
+};
+
+/**
+ * Forbidden Error
+ */
+export class ForbiddenError extends Error {
+    constructor() {
+        super(403, [
+            Messages.FORBIDDEN_ERROR
+        ]);
+    }
+};
+
+/**
+ * NotFound Error
+ */
+export class NotFoundError extends Error {
+    constructor(error: string) {
+        super(404, [
+            error
+        ]);
+    }
+};
+
+/**
+ * InternalServer Error
+ */
+export class InternalServerError extends Error {
+    constructor(error?: string) {
+        super(500, [
+            error ? `Internal Server Error: ${error}` : Messages.INTERNAL_SERVER_ERROR
+        ]);
+    }
+};
+
+/**
+ * ServiceUnavailable Error
+ */
+export class ServiceUnavailableError extends Error {
+    constructor(error?: string) {
+        super(500, [
+            error ? `Service Unavailable: ${error}` : Messages.SERVICE_UNAVAILABLE_ERROR
+        ]);
+    }
+};
---
+++ packages/e-shop-ecommerce-api/src/errors/Messages.ts
@@ -0,0 +1,31 @@
+const Messages = {
+    /**
+     * General Error Messages
+     */
+    UNAUTHORIZED_ERROR: "Authorization Failure: You're not authorized!",
+    FORBIDDEN_ERROR: "Authorization Failure: You're not allowed!",
+    INTERNAL_SERVER_ERROR: "Internal Error: Something went wrong!",
+    SERVICE_UNAVAILABLE_ERROR: "Service Unavailable!",
+
+    /**
+     * Category Error Messages
+     */
+    CATEGORY_NOT_FOUND: "Category Not Found!",
+
+    /**
+     * Country Error Messages
+     */
+    COUNTRY_NOT_FOUND: "Country Not Found!",
+
+    /**
+     * Store Error Messages
+     */
+    STORE_NOT_FOUND: "Store Not Found!",
+
+    /**
+     * Tag Error Messages
+     */
+    TAG_NOT_FOUND: "Tag Not Found!"
+};
+
+export default Messages;
