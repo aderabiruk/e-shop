92f372dd5459f495e31a99739daf91f4cb6274b2
Biruk Adera <aderabiruk@gmail.com>
2020-05-14 12:56:36 +0300

    Product DAL Tests Prepared

---
+++ packages/e-shop-ecommerce-api/test/Generator.ts
@@ -0,0 +1,96 @@
+import Tag from "../src/models/Tag";
+import City from "../src/models/City";
+import Store from "../src/models/Store";
+import Country from '../src/models/Country';
+import Product from "../src/models/Product";
+import Category from "../src/models/Category";
+import PaymentMethod from "../src/models/PaymentMethod";
+import ShipmentMethod from "../src/models/ShipmentMethod";
+
+export const createCategory = (name: string, slug: string, parent: string, image_url: string, description: string) => {
+    let category = new Category();
+    category.name = name;
+    category.slug = slug;
+    category.parent = parent;
+    category.image_url = image_url;
+    category.description = description;
+    return category;
+};
+
+export const createCity = (name: string, code: string, country_id: string, latitude: number, longitude: number) => {
+    let city = new City();
+    city.name = name;
+    city.code = code;
+    city.country_id = country_id;
+    city.location = {
+        type: "Point",
+        coordinates: [ longitude, latitude ]
+    };
+    return city;
+};
+
+export const createCountry = (name: string, code: string, flag: string, currency_name: string, currency_code: string) => {
+    let country = new Country();
+    country.name = name;
+    country.code = code;
+    country.flag = flag;
+    country.currency_name = currency_name;
+    country.currency_code = currency_code;
+    return country;
+};
+
+export const createPaymentMethod = (name: string) => {
+    let paymentMethod = new PaymentMethod();
+    paymentMethod.name = name;
+    return paymentMethod;
+};
+
+export const createShipmentMethod = (name: string) => {
+    let shipmentMethod = new ShipmentMethod();
+    shipmentMethod.name = name;
+    return shipmentMethod;
+};
+
+export const createProduct = (name: string, slug: string, price: number, quantity: number, description: string, image_urls: string[], category_id: string, store_id: string, tags: string[], weight: number, width: number, length: number, height: number, is_visible: boolean, is_out_of_stock: boolean) => {
+    let product = new Product();
+    product.name = name;
+    product.slug = slug;
+    product.price = price;
+    product.quantity = quantity;
+    product.description = description;
+    product.image_urls = image_urls;
+    product.category_id = category_id;
+    product.store_id = store_id;
+    product.tags = tags;
+    product.weight = weight;
+    product.dimension = {
+        width: width,
+        length: length,
+        height: height
+    };
+    product.is_visible = is_visible;
+    product.is_out_of_stock = is_out_of_stock;
+    return product;    
+}
+
+export const createStore = (name: string, email: string, phone_number: string, city_id: string, address: string, latitude: number, longitude: number) => {
+    let store = new Store();
+    store.name = name;
+    store.email = email;
+    store.phone_number = phone_number;
+    store.city_id = city_id;
+    store.address = address;
+    store.location = {
+        type: "Point",
+        coordinates: [ longitude, latitude ]
+    };
+    return store;
+};
+
+export const createTag = (name: string, slug: string, description: string) => {
+    let tag = new Tag();
+    tag.name = name;
+    tag.slug = slug;
+    tag.description = description;
+    return tag;
+};
---
+++ packages/e-shop-ecommerce-api/test/dals/Product.dal.test.ts
@@ -0,0 +1,235 @@
+import mongoose from 'mongoose';
+
+import ProductDAL from '../../src/dals/Product.dal';
+import Product, { Product as IProduct } from '../../src/models/Product';
+import { createProduct } from '../Generator';
+
+const app = require("../../src/app");
+
+describe("Product DAL", () => {
+    
+    describe("create", () => {
+        it("Should return error if required fields are not provieded!", async () => {
+            try {
+                await ProductDAL.create(null, null, null, null ,null, null, null, null, null, null, null, null, null, null, null);
+                fail();
+            }
+            catch (error) {
+                expect(error).toEqual(expect.arrayContaining([
+                    expect.objectContaining({
+                        field: "name",
+                    })
+                ]));
+                expect(error).toEqual(expect.arrayContaining([
+                    expect.objectContaining({
+                        field: "slug",
+                    })
+                ]));
+                expect(error).toEqual(expect.arrayContaining([
+                    expect.objectContaining({
+                        field: "price",
+                    })
+                ]));
+                expect(error).toEqual(expect.arrayContaining([
+                    expect.objectContaining({
+                        field: "quantity",
+                    })
+                ]));
+            }
+        });
+
+        it("Should create store if no errors", async () => {
+            let product = await ProductDAL.create("test-product", "test-product", 100, 100, "test-product-description", [], mongoose.Types.ObjectId().toHexString(), mongoose.Types.ObjectId().toHexString(), [], null, null, null, null, true, false);
+            
+            expect(product.name).toBe("test-product");
+            expect(product.slug).toBe("test-product");
+            expect(product.price).toBe(100);
+            expect(product.quantity).toBe(100);
+            expect(product.description).toBe("test-product-description");
+            expect(product.image_urls.length).toBe(0);
+            expect(product.store_id).toBeTruthy();
+            expect(product.category_id).toBeTruthy();
+            expect(product.tags.length).toBe(0);
+            expect(product.weight).toBe(0);
+            expect(product.dimension.width).toBe(0);
+            expect(product.dimension.length).toBe(0);
+            expect(product.dimension.height).toBe(0);
+            expect(product.is_visible).toBeTruthy();
+            expect(product.is_out_of_stock).toBeFalsy();
+        });
+
+        it("Should create store if no errors", async () => {
+            let product = await ProductDAL.create("test-product", "test-product", 100, 100, "test-product-description", [mongoose.Types.ObjectId().toHexString()], mongoose.Types.ObjectId().toHexString(), mongoose.Types.ObjectId().toHexString(), [mongoose.Types.ObjectId().toHexString()], 250, 10, 10, 10, true, false);
+            
+            expect(product.name).toBe("test-product");
+            expect(product.slug).toBe("test-product");
+            expect(product.price).toBe(100);
+            expect(product.quantity).toBe(100);
+            expect(product.description).toBe("test-product-description");
+            expect(product.image_urls.length).toBe(1);
+            expect(product.store_id).toBeTruthy();
+            expect(product.category_id).toBeTruthy();
+            expect(product.tags.length).toBe(1);
+            expect(product.weight).toBe(250);
+            expect(product.dimension.width).toBe(10);
+            expect(product.dimension.length).toBe(10);
+            expect(product.dimension.height).toBe(10);
+            expect(product.is_visible).toBeTruthy();
+            expect(product.is_out_of_stock).toBeFalsy();
+        });
+    });
+
+    describe("findMany", () => {
+        it("Should return products", async () => {
+            await createProduct("test-product", "test-product", 100, 100, "test-product-description", ["image-url"], mongoose.Types.ObjectId().toHexString(), mongoose.Types.ObjectId().toHexString(), [mongoose.Types.ObjectId().toHexString()], 250, 10, 10, 10, true, false).save();
+
+            let products: IProduct[] = await ProductDAL.findMany({});
+            expect(products.length).toBeGreaterThan(0);
+        });
+
+        it("Should filter products by category (Empty Response)", async () => {
+            await createProduct("test-product", "test-product", 100, 100, "test-product-description", ["image-url"], mongoose.Types.ObjectId().toHexString(), mongoose.Types.ObjectId().toHexString(), [mongoose.Types.ObjectId().toHexString()], 250, 10, 10, 10, true, false).save();
+
+            let products: IProduct[] = await ProductDAL.findMany({category_id: mongoose.Types.ObjectId()});
+            expect(products.length).toBe(0);
+        });
+
+        it("Should filter products by category (With Response)", async () => {
+            let category_id = mongoose.Types.ObjectId();
+            await createProduct("test-product", "test-product", 100, 100, "test-product-description", ["image-url"], category_id.toHexString(), mongoose.Types.ObjectId().toHexString(), [mongoose.Types.ObjectId().toHexString()], 250, 10, 10, 10, true, false).save();
+
+            let products: IProduct[] = await ProductDAL.findMany({ category_id: category_id.toHexString() });
+            expect(products.length).toBe(1);
+        });
+
+        it("Should filter products by store (Empty Response)", async () => {
+            await createProduct("test-product", "test-product", 100, 100, "test-product-description", ["image-url"], mongoose.Types.ObjectId().toHexString(), mongoose.Types.ObjectId().toHexString(), [mongoose.Types.ObjectId().toHexString()], 250, 10, 10, 10, true, false).save();
+
+            let products: IProduct[] = await ProductDAL.findMany({store_id: mongoose.Types.ObjectId()});
+            expect(products.length).toBe(0);
+        });
+
+        it("Should filter products by store (With Response)", async () => {
+            let store_id = mongoose.Types.ObjectId();
+            await createProduct("test-product", "test-product", 100, 100, "test-product-description", ["image-url"], mongoose.Types.ObjectId().toHexString(), store_id.toHexString(), [mongoose.Types.ObjectId().toHexString()], 250, 10, 10, 10, true, false).save();
+
+            let products: IProduct[] = await ProductDAL.findMany({ store_id: store_id.toHexString() });
+            expect(products.length).toBe(1);
+        });
+    });
+
+    describe("findOne", () => {
+        it("Should return null if product doesn't exist", async () => {
+            let product: IProduct = await ProductDAL.findOne({ _id: mongoose.Types.ObjectId() });
+            expect(product).toBeNull();
+        });
+
+        it("Should return product if it exists", async () => {
+            let product = await createProduct("test-product", "test-product", 100, 100, "test-product-description", ["image-url"], mongoose.Types.ObjectId().toHexString(), mongoose.Types.ObjectId().toHexString(), [mongoose.Types.ObjectId().toHexString()], 250, 10, 10, 10, true, false).save();
+            
+            let fetchedProduct: IProduct = await ProductDAL.findOne({ _id: product._id });
+            expect(fetchedProduct.name).toBe("test-product");
+            expect(fetchedProduct.slug).toBe("test-product");
+            expect(fetchedProduct.price).toBe(100);
+            expect(fetchedProduct.quantity).toBe(100);
+            expect(fetchedProduct.description).toBe("test-product-description");
+            expect(fetchedProduct.image_urls.length).toBe(1);
+            expect(fetchedProduct.store_id).toBeTruthy();
+            expect(fetchedProduct.category_id).toBeTruthy();
+            expect(fetchedProduct.tags.length).toBe(1);
+            expect(fetchedProduct.weight).toBe(250);
+            expect(fetchedProduct.dimension.width).toBe(10);
+            expect(fetchedProduct.dimension.length).toBe(10);
+            expect(fetchedProduct.dimension.height).toBe(10);
+            expect(fetchedProduct.is_visible).toBeTruthy();
+            expect(fetchedProduct.is_out_of_stock).toBeFalsy();
+        });
+    });
+
+    describe("update", () => {
+        it("Should return null if product doesn't exist", async () => {
+            let product: IProduct = await ProductDAL.update(null, {});
+            expect(product).toBeNull();
+        });
+
+        it("Shouldn't update if payload is empty", async () => {
+            let product = await createProduct("test-product", "test-product", 100, 100, "test-product-description", ["image-url"], mongoose.Types.ObjectId().toHexString(), mongoose.Types.ObjectId().toHexString(), [mongoose.Types.ObjectId().toHexString()], 250, 10, 10, 10, true, false).save();
+            
+            let updateProduct: IProduct = await ProductDAL.update(product, {  });
+            expect(updateProduct.name).toBe("test-product");
+            expect(updateProduct.slug).toBe("test-product");
+            expect(updateProduct.price).toBe(100);
+            expect(updateProduct.quantity).toBe(100);
+            expect(updateProduct.description).toBe("test-product-description");
+            expect(updateProduct.image_urls.length).toBe(1);
+            expect(updateProduct.store_id).toBeTruthy();
+            expect(updateProduct.category_id).toBeTruthy();
+            expect(updateProduct.tags.length).toBe(1);
+            expect(updateProduct.weight).toBe(250);
+            expect(updateProduct.dimension.width).toBe(10);
+            expect(updateProduct.dimension.length).toBe(10);
+            expect(updateProduct.dimension.height).toBe(10);
+            expect(updateProduct.is_visible).toBeTruthy();
+            expect(updateProduct.is_out_of_stock).toBeFalsy();
+        });
+
+        it("Should update product", async () => {
+            let product = await createProduct("test-product", "test-product", 100, 100, "test-product-description", ["image-url"], mongoose.Types.ObjectId().toHexString(), mongoose.Types.ObjectId().toHexString(), [mongoose.Types.ObjectId().toHexString()], 250, 10, 10, 10, true, false).save();
+            
+            let updateProduct: IProduct = await ProductDAL.update(product, { description: "test-product-description-new", weight: 400, width: 20, image_urls: [] });
+            expect(updateProduct.name).toBe("test-product");
+            expect(updateProduct.slug).toBe("test-product");
+            expect(updateProduct.price).toBe(100);
+            expect(updateProduct.quantity).toBe(100);
+            expect(updateProduct.description).toBe("test-product-description-new");
+            expect(updateProduct.image_urls.length).toBe(0);
+            expect(updateProduct.store_id).toBeTruthy();
+            expect(updateProduct.category_id).toBeTruthy();
+            expect(updateProduct.tags.length).toBe(1);
+            expect(updateProduct.weight).toBe(400);
+            expect(updateProduct.dimension.width).toBe(20);
+            expect(updateProduct.dimension.length).toBe(10);
+            expect(updateProduct.dimension.height).toBe(10);
+            expect(updateProduct.is_visible).toBeTruthy();
+            expect(updateProduct.is_out_of_stock).toBeFalsy();
+        });
+    });
+
+    describe("deleteHard", () => {
+        it("Should return 0 deletedCount if product doesn't exist", async () => {
+            let result: any = await ProductDAL.deleteHard({ _id: mongoose.Types.ObjectId().toHexString() });
+            expect(result.deletedCount).toBe(0);
+        });
+
+        it("Should delete product if it exists", async () => {
+            let product = await createProduct("test-product", "test-product", 100, 100, "test-product-description", ["image-url"], mongoose.Types.ObjectId().toHexString(), mongoose.Types.ObjectId().toHexString(), [mongoose.Types.ObjectId().toHexString()], 250, 10, 10, 10, true, false).save();
+
+            let result: any = await ProductDAL.deleteHard({ _id: product._id.toHexString() });
+            expect(result.deletedCount).toBe(1);
+        });
+    });
+
+    describe("deleteSoft", () => {
+        it("Should return null if null product is passed", async () => {
+            let product: IProduct = await ProductDAL.deleteSoft(null);
+            expect(product).toBeNull();
+        });
+
+        it("Should soft delete product if it exists", async () => {
+            let product = await createProduct("test-product", "test-product", 100, 100, "test-product-description", ["image-url"], mongoose.Types.ObjectId().toHexString(), mongoose.Types.ObjectId().toHexString(), [mongoose.Types.ObjectId().toHexString()], 250, 10, 10, 10, true, false).save();
+
+            let deletedStore: IProduct = await ProductDAL.deleteSoft(product);
+            expect(deletedStore).not.toBeNull();
+            expect(deletedStore.deleted_at).toBeTruthy();
+        });
+    });
+
+    afterEach(async () => {
+        await Product.deleteMany({"name": "test-product"});
+    });
+
+    afterAll(async () => {
+        await mongoose.connection.close();
+    });
+
+});
