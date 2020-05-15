bc693ec478a5579fd1c3afaa288fd2bb7a18ae1e
Biruk Adera <aderabiruk@gmail.com>
2020-05-15 14:07:48 +0300

    Payment DAL and Shipment DAL Tests Modified

--- packages/e-shop-ecommerce-api/src/utilities/constants/General.ts
+++ packages/e-shop-ecommerce-api/src/utilities/constants/General.ts
@@ -5,7 +5,7 @@ export const Gender = {
 };
 
 export const OrderStatus = {
-    PENDING: "PENDING",
+    PENDING: "Pending",
     PROCESSING: "Processing",
     READY_FOR_SHIPMENT: "Ready_For_Shipment",
     IN_TRANSIT: "In_Transit",
@@ -13,4 +13,19 @@ export const OrderStatus = {
     CANCELLED: "Cancelled",
     REJECTED: "Rejected",
     REFUNDED: "Refended"
+};
+
+export const PaymentStatus = {
+    PENDING: "Pending",
+    COMPLETED: "Completed",
+    CANCELLED: "Cancelled",
+    REJECTED: "Rejected",
+    DECLINED: "Declined"
+};
+
+export const ShipmentStatus = {
+    PENDING: "Pending",
+    SHIPPED: "Shipped",
+    CANCELLED: "Cancelled",
+    IN_TRANSIT: "In_Transit",
 };
