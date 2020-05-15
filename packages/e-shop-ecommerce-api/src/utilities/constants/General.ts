f752988797aa0e6b1941fb5d794f1653549fde24
Biruk Adera <aderabiruk@gmail.com>
2020-05-15 12:18:45 +0300

    OrderStatus Added

---
+++ packages/e-shop-ecommerce-api/src/utilities/constants/General.ts
@@ -0,0 +1,16 @@
+export const Gender = {
+    MALE: "Male",
+    FEMALE: "Female",
+    OTHER: "Other"
+};
+
+export const OrderStatus = {
+    PENDING: "PENDING",
+    PROCESSING: "Processing",
+    READY_FOR_SHIPMENT: "Ready_For_Shipment",
+    IN_TRANSIT: "In_Transit",
+    COMPLETED: "Completed",
+    CANCELLED: "Cancelled",
+    REJECTED: "Rejected",
+    REFUNDED: "Refended"
+};
