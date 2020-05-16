47f7184dde23acb99b4c910432e39c3d053a010b
Biruk Adera <aderabiruk@gmail.com>
2020-05-16 19:35:26 +0300

    Error Messages Prepared

--- packages/e-shop-ecommerce-api/src/errors/Messages.ts
+++ packages/e-shop-ecommerce-api/src/errors/Messages.ts
@@ -14,9 +14,23 @@ const Messages = {
     CATEGORY_NAME_REQUIRED: "Category name is required!",
 
     /**
+     * City Error Messages
+     */
+    CITY_NOT_FOUND: "City Not Found!",
+    CITY_NAME_REQUIRED: "City name is required!",
+    CITY_CODE_REQUIRED: "City code is required!",
+    CITY_COUNTRY_REQUIRED: "Country is required!",
+    CITY_LOCATION_REQUIRED: "City location is required!",
+
+    /**
      * Country Error Messages
      */
     COUNTRY_NOT_FOUND: "Country Not Found!",
+    COUNTRY_NAME_REQUIRED: "Country name is required!",
+    COUNTRY_CODE_REQUIRED: "Country code is required!",
+    COUNTRY_FLAG_REQUIRED: "Country flag is required!",
+    COUNTRY_CURRENCY_NAME_REQUIRED: "Country currency name is required!",
+    COUNTRY_CURRENCY_CODE_REQUIRED: "Country currency code is required!",
 
     /**
      * Discout Error Messages
@@ -29,14 +43,33 @@ const Messages = {
     ORDER_ITEMS_REQUIRED: "Items are required!",
 
     /**
+     * Payment Method Error Messages
+     */
+    PAYMENT_METHOD_NOT_FOUND: "Payment method not found!",
+    PAYMENT_METHOD_NAME_REQUIRED: "Payment method name required!",
+
+    /**
+     * Shipment Method Error Messages
+     */
+    SHIPMENT_METHOD_NOT_FOUND: "Shipment method not found!",
+    SHIPMENT_METHOD_NAME_REQUIRED: "Shipment method name required!",
+
+    /**
      * Store Error Messages
      */
     STORE_NOT_FOUND: "Store Not Found!",
+    STORE_NAME_REQUIRED: "Store name is required!",
+    STORE_EMAIL_REQUIRED: "Store email is required!",
+    STORE_PHONE_NUMBER_REQUIRED: "Store phone number is required!",
+    STORE_CITY_REQUIRED: "City is required!",
+    STORE_ADDRESS_REQUIRED: "Store address is required!",
+    STORE_LOCATION_REQUIRED: "Store location is required!",
 
     /**
      * Tag Error Messages
      */
-    TAG_NOT_FOUND: "Tag Not Found!"
+    TAG_NOT_FOUND: "Tag Not Found!",
+    TAG_NAME_REQUIRED: "Tag name is required!"
 };
 
 export default Messages;
