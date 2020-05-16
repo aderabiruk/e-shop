727dffe0cd6e2ebb65360c2d314c2b6c5b1fe121
Biruk Adera <aderabiruk@gmail.com>
2020-05-16 18:26:58 +0300

    City Service Model

---
+++ packages/e-shop-ecommerce-api/src/services/City.service.ts
@@ -0,0 +1,183 @@
+import async from 'async';
+import mongoose from 'mongoose';
+import evalidate from 'evalidate';
+
+import { ICity } from '../models/City';
+import CityDAL from '../dals/City.dal';
+import Messages from '../errors/Messages';
+import { ICountry } from '../models/Country';
+import CountryService from './Country.service';
+import { NotFoundError, BadRequestError, InternalServerError } from '../errors/Errors';
+import { IPaginationResponse, PaginationAdapter } from '../utilities/adapters/Pagination';
+
+class CityService {
+    /**
+     * Create City
+     * 
+     * @param {string} name
+     * @param {string} code
+     * @param {string} country
+     * @param {number} latitude
+     * @param {number} longitude
+     * 
+     * @returns {Promise<ICity>}
+     */
+    static create(name: string, code: string, country: string, latitude: number, longitude: number): Promise<ICity> {
+        return new Promise((resolve, reject) => {
+            async.waterfall([
+                (done: Function) => {
+                    const Schema = new evalidate.schema({
+                        name: evalidate.string().required(Messages.CITY_NAME_REQUIRED),
+                        code: evalidate.string().required(Messages.CITY_CODE_REQUIRED),
+                        country: evalidate.string().required(Messages.CITY_COUNTRY_REQUIRED),
+                        latitude: evalidate.number().required(Messages.CITY_LOCATION_REQUIRED),
+                        longitude: evalidate.number().required(Messages.CITY_LOCATION_REQUIRED),
+                    });
+                    const result = Schema.validate({ name: name, code: code, country: country, latitude: latitude, longitude: longitude });
+                    if (result.isValid) {
+                        done(null);
+                    }
+                    else {
+                        done(new BadRequestError(result.errors));
+                    }
+                },
+                (done: Function) => {
+                    CountryService.findByID(country)
+                        .then((country: ICountry) => {
+                            if (!country) {
+                                done(new BadRequestError([ { field: "country", message: Messages.COUNTRY_NOT_FOUND } ]));
+                            }
+                            else {
+                                done(null, country);
+                            }
+                        })
+                        .catch((error: any) => {
+                            done(new InternalServerError(error));
+                        });
+                },
+                (country: ICountry, done: Function) => {
+                    CityDAL.create(name, code, country._id, latitude, longitude)
+                        .then((city: ICity) => {
+                            resolve(city);
+                        })
+                        .catch((error: any) => {
+                            done(new BadRequestError(error));
+                        });
+                }
+            ], (error: any) => {
+                if (error) {
+                    reject(error);
+                }
+            });
+        });
+    }
+
+    /**
+     * Find All Cities
+     * 
+     * @param {number} page
+     * @param {number} limit
+     * 
+     * @returns {Promise<IPaginationResponse[]>}
+     */
+    static findAll(page: number = 1, limit: number = 25): Promise<IPaginationResponse> {
+        return new Promise((resolve, reject) => {
+            CityDAL.findMany({ deleted_at: null }, page, limit)
+                .then((cities: ICity[]) => {
+                    resolve(PaginationAdapter(cities, page, limit));
+                })
+                .catch((error) => {
+                    reject(new InternalServerError(error));
+                });
+
+        });
+    }
+
+    /**
+     * Find City By ID
+     * 
+     * @param {number} page
+     * @param {number} limit
+     * 
+     * @returns {Promise<ICity[]>}
+     */
+    static findByID(id: string): Promise<ICity> {
+        return new Promise((resolve, reject) => {
+            if (!mongoose.isValidObjectId(id)) {
+                resolve(null);
+            }
+            else {
+                CityDAL.findOne({ _id: id })
+                    .then((city) => {
+                        resolve(city);
+                    })
+                    .catch((error) => {
+                        reject(new InternalServerError(error));
+                    });
+            }
+        });
+    }
+
+    /**
+     * Update City By ID
+     * 
+     * @param {string}  id 
+     * @param {any}     payload 
+     * 
+     * @returns {Promise<ICity>}
+     */
+    static update(id: string, payload: any = {}): Promise<ICity> {
+        return new Promise((resolve, reject) => {
+            async.waterfall([
+                (done: Function) => {
+                    CityService.findByID(id)
+                        .then((city: ICity) => {
+                            if (city) {
+                                done(null, city);
+                            }
+                            else {
+                                done(new NotFoundError(Messages.CITY_NOT_FOUND));
+                            }
+                        })
+                        .catch((error: any) => {
+                            done(error);
+                        });
+                },
+                (city: ICity, done: Function) => {
+                    if (payload && payload.country) {
+                        CountryService.findByID(payload.country)
+                            .then((country: ICountry) => {
+                                if (country) {
+                                    done(null, city, country);
+                                }
+                                else {
+                                    done(new BadRequestError([ { field: "country", message: Messages.COUNTRY_NOT_FOUND } ]));
+                                }
+                            })
+                            .catch((error: any) => {
+                                done(error);
+                            });
+                    }
+                    else {
+                        done(null, city, null);
+                    }
+                },
+                (city: ICity, country: ICountry, done: Function) => {
+                    CityDAL.update(city, payload)
+                        .then((updatedCity: ICity) => {
+                            resolve(updatedCity);
+                        })
+                        .catch((error: any) => {
+                            done(error);
+                        })
+                }
+            ], (error: any) => {
+                if (error) {
+                    reject(error);
+                }
+            });
+        });
+    }
+};
+
+export default CityService;
---
+++ packages/e-shop-ecommerce-api/test/services/City.service.test.ts
@@ -0,0 +1,213 @@
+import mongoose from 'mongoose';
+
+import { createCity, createCountry } from '../Generator';
+import Messages from '../../src/errors/Messages';
+import City, { ICity } from '../../src/models/City';
+import Country, { ICountry } from '../../src/models/Country';
+import CityService from '../../src/services/City.service';
+import { IPaginationResponse } from '../../src/utilities/adapters/Pagination';
+
+const app = require("../../src/app");
+
+const LATITUDE = 125;
+const LONGITUDE = 99;
+
+describe("City.service", () => {
+    let country: ICountry;
+
+    beforeAll(async () => {
+        country = await createCountry("test-country", "test-country", "test-country", "test-country", "test-country").save();
+    });
+    
+    describe("create", () => {
+        it("Should return validation error if required fields are not provided", async () => {
+            try {
+                await CityService.create(null, null, null, null ,null);
+                fail();
+            }
+            catch (error) {
+                expect(error.statusCode).toBe(400);
+                expect(error.payload.errors).toEqual(expect.arrayContaining([
+                    expect.objectContaining({
+                        field: "name",
+                        message: Messages.CITY_NAME_REQUIRED
+                    })
+                ]));
+                expect(error.payload.errors).toEqual(expect.arrayContaining([
+                    expect.objectContaining({
+                        field: "code",
+                        message: Messages.CITY_CODE_REQUIRED
+                    })
+                ]));
+                expect(error.payload.errors).toEqual(expect.arrayContaining([
+                    expect.objectContaining({
+                        field: "country",
+                        message: Messages.CITY_COUNTRY_REQUIRED
+                    })
+                ]));
+                expect(error.payload.errors).toEqual(expect.arrayContaining([
+                    expect.objectContaining({
+                        field: "latitude",
+                        message: Messages.CITY_LOCATION_REQUIRED
+                    })
+                ]));
+                expect(error.payload.errors).toEqual(expect.arrayContaining([
+                    expect.objectContaining({
+                        field: "longitude",
+                        message: Messages.CITY_LOCATION_REQUIRED
+                    })
+                ]));
+            }
+        });
+
+        it("Should return error if country doesn't exist", async () => {
+            try {
+                await CityService.create("test-city", "test-city-code", mongoose.Types.ObjectId().toHexString(), LATITUDE, LONGITUDE);
+                fail();
+            }
+            catch (error) {
+                expect(error.statusCode).toBe(400);
+                expect(error.payload.errors).toEqual(expect.arrayContaining([
+                    expect.objectContaining({
+                        field: "country",
+                        message: Messages.COUNTRY_NOT_FOUND
+                    })
+                ]));
+            }
+        });
+
+        it("Should create city if no errors!", async () => {
+            let city = await CityService.create("test-city", "test-city-code", country._id.toHexString(), LATITUDE, LONGITUDE);
+            
+            expect(city.name).toBe("test-city");
+            expect(city.code).toBe("test-city-code");
+            expect(city.location.type).toBe("Point");
+            expect(city.location.coordinates).toContainEqual(LATITUDE);
+            expect(city.location.coordinates).toContainEqual(LONGITUDE);
+        });
+    });
+
+    describe("findMany", () => {
+        it("Should return cities", async () => {
+            await createCity("test-city", "test-city-code", mongoose.Types.ObjectId().toHexString(), LATITUDE, LONGITUDE).save();
+
+            let response: IPaginationResponse = await CityService.findAll();
+            expect(response.data.length).toBeGreaterThan(0);
+            expect(response.metadata.pagination.page).toBe(1);
+            expect(response.metadata.pagination.limit).toBe(25);
+        });
+    });
+
+    describe("findOne", () => {
+        it("Should return null if id is invalid", async () => {
+            let city: ICity = await CityService.findByID("INVALID-ID");
+            expect(city).toBeNull();
+        });
+
+        it("Should return null if city doesn't exist", async () => {
+            let city: ICity = await CityService.findByID(mongoose.Types.ObjectId().toHexString());
+            expect(city).toBeNull();
+        });
+
+        it("Should return city", async () => {
+            let city: ICity = await createCity("test-city", "test-city-code", mongoose.Types.ObjectId().toHexString(), LATITUDE, LONGITUDE).save();
+
+            let fetchedCity: ICity = await CityService.findByID(city._id );
+            expect(fetchedCity.name).toBe("test-city");
+            expect(fetchedCity.code).toBe("test-city-code");
+            expect(fetchedCity.location.type).toBe("Point");
+            expect(fetchedCity.location.coordinates).toContainEqual(LATITUDE);
+            expect(fetchedCity.location.coordinates).toContainEqual(LONGITUDE);
+        });
+    });
+
+    describe("update", () => {
+        it("Should return error if city doesn't exist", async () => {
+            try {
+                let city: ICity = await CityService.update("INVALID-ID", {});
+                fail();
+            }
+            catch (error) {
+                expect(error.statusCode).toBe(404);
+                expect(error.payload.errors).toEqual(expect.arrayContaining([
+                    Messages.CITY_NOT_FOUND
+                ]));
+            }
+        });
+
+        it("Should return error if country doesn't exist", async () => {
+            let city: ICity = await createCity("test-city", "test-city-code", mongoose.Types.ObjectId().toHexString(), LATITUDE, LONGITUDE).save();
+
+            try {
+                await CityService.update(city._id, { country: "INVALID-ID" });
+                fail();
+            }
+            catch (error) {
+                expect(error.statusCode).toBe(400);
+                expect(error.payload.errors).toEqual(expect.arrayContaining([
+                    expect.objectContaining({
+                        field: "country",
+                        message: Messages.COUNTRY_NOT_FOUND
+                    })
+                ]));
+            }
+        });
+
+        it("Should update if payload is provided", async () => {
+            let city: ICity = await createCity("test-city", "test-city-code", mongoose.Types.ObjectId().toHexString(), LATITUDE, LONGITUDE).save();
+
+            let updatedCity: ICity = await CityService.update(city._id, { code: "test-city-code-1" });
+            expect(updatedCity.name).toBe("test-city");
+            expect(updatedCity.code).toBe("test-city-code-1");
+            expect(updatedCity.location.type).toBe("Point");
+            expect(updatedCity.location.coordinates).toContainEqual(LATITUDE);
+            expect(updatedCity.location.coordinates).toContainEqual(LONGITUDE);
+        });
+
+        it("Shouldn't update location if only latitude or longitude is provided", async () => {
+            let city: ICity = await createCity("test-city", "test-city-code", mongoose.Types.ObjectId().toHexString(), LATITUDE, LONGITUDE).save();
+
+            let updatedCity: ICity = await CityService.update(city._id, { latitude: 500 });
+            expect(updatedCity.name).toBe("test-city");
+            expect(updatedCity.code).toBe("test-city-code");
+            expect(updatedCity.location.type).toBe("Point");
+            expect(updatedCity.location.coordinates).toContainEqual(LATITUDE);
+            expect(updatedCity.location.coordinates).toContainEqual(LONGITUDE);
+        });
+
+        it("Shouldn't update location if latitude or longitude is not a number", async () => {
+            let city: ICity = await createCity("test-city", "test-city-code", mongoose.Types.ObjectId().toHexString(), LATITUDE, LONGITUDE).save();
+
+            try {
+                let updatedCity: ICity = await CityService.update(city._id, { latitude: "invalid", longitude: "invalid" });
+                fail();
+            }
+            catch (error) {
+                expect(error).toBeTruthy();
+            }
+        });
+
+        it("Should update location if both latitude and longitude are provided", async () => {
+            let city: ICity = await createCity("test-city", "test-city-code", mongoose.Types.ObjectId().toHexString(), LATITUDE, LONGITUDE).save();
+
+            let updatedCity: ICity = await CityService.update(city._id, { latitude: 500, longitude: 500 });
+            expect(updatedCity.name).toBe("test-city");
+            expect(updatedCity.code).toBe("test-city-code");
+            expect(updatedCity.location.type).toBe("Point");
+            expect(updatedCity.location.coordinates).toContainEqual(500);
+            expect(updatedCity.location.coordinates).toContainEqual(500);
+        });
+
+    });
+
+    afterEach(async () => {
+        await City.deleteMany({ name: "test-city"}); 
+    });
+
+    afterAll(async () => {
+        await Country.deleteMany({ name: "test-country"});
+
+        await mongoose.connection.close();
+    });
+
+});
