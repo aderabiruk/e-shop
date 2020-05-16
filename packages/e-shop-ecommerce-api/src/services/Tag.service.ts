c9f8ae89c7ef56339b4d30796dd3b1b5af4c2725
Biruk Adera <aderabiruk@gmail.com>
2020-05-16 19:34:16 +0300

    Tag Services Prepared

---
+++ packages/e-shop-ecommerce-api/src/services/Tag.service.ts
@@ -0,0 +1,145 @@
+import voca from 'voca';
+import async from 'async';
+import mongoose from 'mongoose';
+import evalidate from 'evalidate';
+
+import { ITag } from '../models/Tag';
+import TagDAL from '../dals/Tag.dal';
+import Messages from '../errors/Messages';
+import { NotFoundError, BadRequestError, InternalServerError } from '../errors/Errors';
+import { IPaginationResponse, PaginationAdapter } from '../utilities/adapters/Pagination';
+
+
+class TagService {
+    
+    /**
+     * Create Tag
+     * 
+     * @param {string} name
+     * @param {string} description
+     * 
+     * @returns {Promise<ITag>}
+     */
+    static create(name: string, description: string): Promise<ITag> {
+        return new Promise((resolve, reject) => {
+            async.waterfall([
+                (done: Function) => {
+                    const Schema = new evalidate.schema({
+                        name: evalidate.string().required(Messages.TAG_NAME_REQUIRED)
+                    });
+                    const result = Schema.validate({ name: name });
+                    if (result.isValid) {
+                        done(null);
+                    }
+                    else {
+                        done(new BadRequestError(result.errors));
+                    }
+                },
+                (done: Function) => {
+                    let slug = voca.slugify(name);
+                    TagDAL.create(name, slug, description)
+                        .then((tag: ITag) => {
+                            resolve(tag);
+                        })
+                        .catch((error: any) => {
+                            done(new BadRequestError(error));
+                        });
+                }
+            ], (error: any) => {
+                if (error) {
+                    reject(error);
+                }
+            })
+        });
+    }
+
+    /**
+     * Find All Tags
+     * 
+     * @param {number} page
+     * @param {number} limit
+     * 
+     * @returns {Promise<IPaginationResponse[]>}
+     */
+    static findAll(page: number = 1, limit: number = 25): Promise<IPaginationResponse> {
+        return new Promise((resolve, reject) => {
+            TagDAL.findMany({ deleted_at: null }, page, limit)
+                .then((tags: ITag[]) => {
+                    resolve(PaginationAdapter(tags, page, limit));
+                })
+                .catch((error) => {
+                    reject(new InternalServerError(error));
+                });
+
+        });
+    }
+
+    /**
+     * Find Tag By ID
+     * 
+     * @param {number} page
+     * @param {number} limit
+     * 
+     * @returns {Promise<ITag[]>}
+     */
+    static findByID(id: string): Promise<ITag> {
+        return new Promise((resolve, reject) => {
+            if (!mongoose.isValidObjectId(id)) {
+                resolve(null);
+            }
+            else {
+                TagDAL.findOne({ _id: id })
+                    .then((tag) => {
+                        resolve(tag);
+                    })
+                    .catch((error) => {
+                        reject(new InternalServerError(error));
+                    });
+            }
+        });
+    }
+
+    /**
+     * Update Tag By ID
+     * 
+     * @param {string}  id 
+     * @param {any}     payload 
+     * 
+     * @returns {Promise<ITag>}
+     */
+    static update(id: string, payload: any = {}): Promise<ITag> {
+        return new Promise((resolve, reject) => {
+            async.waterfall([
+                (done: Function) => {
+                    TagService.findByID(id)
+                        .then((tag: ITag) => {
+                            if (tag) {
+                                done(null, tag);
+                            }
+                            else {
+                                done(new NotFoundError(Messages.TAG_NOT_FOUND));
+                            }
+                        })
+                        .catch((error: any) => {
+                            done(error);
+                        });
+                },
+                (tag: ITag, done: Function) => {
+                    TagDAL.update(tag, payload)
+                        .then((updatedTag: ITag) => {
+                            resolve(updatedTag);
+                        })
+                        .catch((error: any) => {
+                            done(error);
+                        })
+                }
+            ], (error: any) => {
+                if (error) {
+                    reject(error);
+                }
+            });
+        });
+    }
+};
+
+export default TagService;
---
+++ packages/e-shop-ecommerce-api/test/services/Tag.service.test.ts
@@ -0,0 +1,115 @@
+import mongoose from 'mongoose';
+
+import { createTag } from '../Generator';
+import Messages from '../../src/errors/Messages';
+import Tag, { ITag } from '../../src/models/Tag';
+import TagService from '../../src/services/Tag.service';
+import { IPaginationResponse } from '../../src/utilities/adapters/Pagination';
+
+const app = require("../../src/app");
+
+describe("Tag.service", () => {
+        
+    describe("create", () => {
+        it("Should return validation error if name is not provided", async () => {
+            try {
+                await TagService.create(null, null);
+                fail();
+            }
+            catch (error) {
+                expect(error.statusCode).toBe(400);
+                expect(error.payload.errors).toEqual(expect.arrayContaining([
+                    expect.objectContaining({
+                        field: "name",
+                        message: Messages.TAG_NAME_REQUIRED
+                    })
+                ]));
+            }
+        });
+
+        it("Should create tag ", async () => {
+            let tag = await TagService.create("test-tag", "test-tag");
+            expect(tag.name).toBe("test-tag");
+            expect(tag.slug).toBe("test-tag");
+            expect(tag.description).toBe("test-tag");
+        });
+
+    });
+
+    describe("findMany", () => {
+        it("Should return tags", async () => {
+            await createTag("test-tag", "test-tag", "test-tag").save();
+
+            let response: IPaginationResponse = await TagService.findAll();
+            expect(response.data.length).toBeGreaterThan(0);
+            expect(response.metadata.pagination.page).toBe(1);
+            expect(response.metadata.pagination.limit).toBe(25);
+        });
+    });
+
+    describe("findByID", () => {
+        it("Should return null if id is invalid", async () => {
+            let tag: ITag = await TagService.findByID("INVALID-ID");
+            expect(tag).toBeNull();
+        });
+
+        it("Should return null if tag doesn't exist", async () => {
+            let tag: ITag = await TagService.findByID(mongoose.Types.ObjectId().toHexString());
+            expect(tag).toBeNull();
+        });
+
+        it("Should return tag if it exist", async () => {
+            let Tag: ITag = await createTag("test-tag", "test-tag", "test-tag").save();
+
+            let fetchedTag: ITag = await TagService.findByID(Tag._id);
+            expect(fetchedTag).not.toBeNull();
+            expect(fetchedTag.name).toBe("test-tag");
+            expect(fetchedTag.slug).toBe("test-tag");
+            expect(fetchedTag.description).toBe("test-tag");
+        });
+    });
+
+    describe("update", () => {
+        it("Should return null if tag doesn't exist", async () => {
+            try {
+                let tag: ITag = await TagService.update(null, {});
+                fail();
+            }
+            catch (error) {
+                expect(error.statusCode).toBe(404);
+                expect(error.payload.errors).toEqual(expect.arrayContaining([
+                    Messages.TAG_NOT_FOUND
+                ]));
+            }
+        });
+
+        it("Shouldn't update if payload is empty", async () => {
+            let tag: ITag = await createTag("test-tag", "test-tag", "test-tag").save();
+
+            let updatedTag: ITag = await TagService.update(tag._id, {});
+            expect(updatedTag).not.toBeNull();
+            expect(updatedTag.name).toBe("test-tag");
+            expect(updatedTag.slug).toBe("test-tag");
+            expect(updatedTag.description).toBe("test-tag");
+        });
+
+        it("Shouldn update if tag description", async () => {
+            let Tag: ITag = await createTag("test-tag", "test-tag", "test-tag").save();
+
+            let updatedTag: ITag = await TagService.update(Tag._id, { description: 'test-tag-2' });
+            expect(updatedTag).not.toBeNull();
+            expect(updatedTag.name).toBe("test-tag");
+            expect(updatedTag.slug).toBe("test-tag");
+            expect(updatedTag.description).toBe("test-tag-2");
+        });
+    });
+
+    afterEach(async () => {
+        await Tag.deleteMany({ name: "test-tag"});
+    });
+
+    afterAll(async () => {
+        await mongoose.connection.close();
+    });
+
+});
