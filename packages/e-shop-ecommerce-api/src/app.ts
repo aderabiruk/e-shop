0de5acc1d768d8ae5446b4f7e6d6d524fb2ffcb9
Biruk Adera <aderabiruk@gmail.com>
2020-05-31 12:26:03 +0300

    Global Error Handler Added for Express

--- packages/e-shop-ecommerce-api/src/app.ts
+++ packages/e-shop-ecommerce-api/src/app.ts
@@ -1,10 +1,16 @@
 import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
+import moment from 'moment';
 import bodyParser from "body-parser";
 import compression from 'compression';
-import express, { Application } from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import express, { Application, Request, Response } from 'express';
 
+import routes from './routers';
+import Schema from './typeDefs';
+import Messages from './errors/Messages';
+import { Error } from './errors/Errors';
 import initializeDB from './utilities/database/mongoose';
 
 /**
@@ -31,5 +37,29 @@ initializeDB();
 /**
  * Initialize Routes
  */
+routes(app);
+
+/**
+ * Global Error Handler
+ */
+app.use((error: any, request: Request, response: Response, next: Function) => {
+    if (error instanceof Error) {
+        response.status(error.statusCode).json(error.payload);
+    }
+    else {
+        response.status(500).json({
+            timestamp: moment(),
+            errors: [
+                Messages.INTERNAL_SERVER_ERROR
+            ]
+        });
+    }
+});
+
+/**
+ * Initialize GraphQL
+ */
+const apollo: ApolloServer = new ApolloServer({ schema : Schema });
+apollo.applyMiddleware({ app });
 
 export default app;
